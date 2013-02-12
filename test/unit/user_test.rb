6840b0981b6550fe43b0e7af85610c6260deab5b
Jimmy Zhang <jzhang621@gmail.com>
2013-02-11 16:45:43 -0800

    edited unitTests

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -23,18 +23,22 @@ class UserTest < ActiveSupport::TestCase
     end
 
     test "add not valid with username over 128 characters" do
+        User.TESTAPI_resetFixture
         assert User.add("a"*129, "pass") == $ERR_BAD_USERNAME, "Adding new user with empty username does not return proper error code"
     end
 
     test "add not valid with password over 128 characters" do
+        User.TESTAPI_resetFixture
         assert User.add("b", "b"*129) == $ERR_BAD_PASSWORD
     end
 
     test "add not valid with empty username" do
+        User.TESTAPI_resetFixture
         assert User.add("", "qwerty123") == $ERR_BAD_USERNAME
     end
 
     test "add not valid with exisiting username" do
+        User.TESTAPI_resetFixture
         assert User.add("jimmy", "password") == $SUCCESS
         assert User.add("jimmy", "password") == $ERR_USER_EXISTS
     end
@@ -46,15 +50,18 @@ class UserTest < ActiveSupport::TestCase
     end
 
     test "add and then login" do
+        User.TESTAPI_resetFixture
         assert User.add("user", "pass") == 1
         assert User.login("user", "pass") == 2
     end
 
     test "login with bad credentials" do
+        User.TESTAPI_resetFixture
         assert User.login("random", "password") == $ERR_BAD_CREDENTIALS
     end
 
     test "TESTAPI_resetFixture" do
+        User.TESTAPI_resetFixture
         assert User.TESTAPI_resetFixture == $SUCCESS
     end
 
